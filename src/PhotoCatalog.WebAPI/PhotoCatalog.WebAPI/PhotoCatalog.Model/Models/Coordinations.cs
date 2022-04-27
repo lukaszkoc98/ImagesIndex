483f8cb0a9c82d71839ec6305c489b3bc7c9a65f
TomaszMaciazek <tommac.opole@gmail.com>
2022-04-27 19:20:33 +0200

    Added coordinations class

---
+++ src/PhotoCatalog.WebAPI/PhotoCatalog.WebAPI/PhotoCatalog.Model/Models/Coordinations.cs
@@ -0,0 +1,9 @@
+﻿namespace PhotoCatalog.Model.Models
+{
+    public class Coordinations
+    {
+        public float Degrees { get; set; }
+        public float Minutes { get; set; }
+        public float Seconds { get; set; }
+    }
+}
